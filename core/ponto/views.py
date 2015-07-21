e60ae93ed707106f3d8d166b79690d926048c9ed
Fabio <nanook@nanook.com.br>
2015-07-21 17:01:09 -0300

    mais correções do separador decimal (maldito javascript)

--- core/ponto/views.py
+++ core/ponto/views.py
@@ -120,7 +120,7 @@ def cria_relatorio(id_inicio, id_fim, usuario):
             _saldo += e.total_horas
         else:
             _saldo += (e.total_horas - 8.0)
-        saldo_graf.append({"dia": e.dia, "saldo": _saldo})
+        saldo_graf.append({"dia": e.dia, "saldo": ("%.2f" % _saldo).replace(",", ".")})
 
     saldo = total_extra - total_deficit
     saldo_string = Entrada.format_time(saldo)  # "%02d:%02d:00" % (int(saldo.total_seconds()/60/60), int((abs(saldo.total_seconds()) - abs(int(saldo.total_seconds()/60/60)*60*60))/60) )
