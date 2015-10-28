907659dc00b55917084b5afbabf198ac5b0ceb38
Fabio Pachelli Pacheco <nanook.labs@gmail.com>
2015-10-28 10:56:12 -0200

    Log de registro

--- core/ponto/views.py
+++ core/ponto/views.py
@@ -67,10 +67,13 @@ def home(request):
 
 @staff_member_required
 def registrar(request, momento):
+    l = open("/var/log/http/log_de_registro.log", "a")
     agent = request.environ.get("HTTP_USER_AGENT")
     ip = request.environ.get("REMOTE_ADDR")
     user = "%s - %s" % (request.user.id, request.user.username)
     print "%s - %s - %s - %s - %s" % (datetime.now(), momento, user, ip, agent)
+    l.write("%s - %s - %s - %s - %s" % (datetime.now(), momento, user, ip, agent))
+    l.clode()
     usuario = request.user
     entrada = Entrada.objects.get_or_none(usuario=usuario, dia=datetime.today())
     if not entrada:
